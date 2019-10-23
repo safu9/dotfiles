2c931b48fb76e712575d16a02b2757cda1f98b11
safu9 <safu9.dev@gmail.com>
2019-10-23 13:52:18 +0900

    Add install script

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # dotfiles
+
+## Install
+
+In your home directory,
+```bash
+git clone git@github.com:safu9/dotfiles.git && dotfiles/install.sh
+```
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Installing dotfiles..."
+
+DOTPATH="$HOME/dotfiles"
+
+cd $DOTPATH
+for file in .??*; do
+    [ "$file" = ".git" ] && continue
+
+    if [ -L "$HOME/$file" ]; then
+        ln -snfv "$DOTPATH/$file" "$HOME/$file"
+    else
+        # if file already exists, save backup.
+        ln -snfbv "$DOTPATH/$file" "$HOME/$file"
+    fi
+done
+
+echo "Complete!"
